6430b2634f5a845a05a1be6a5eb0fa35bfd37f83
Thomas Ardal <thomasardal@gmail.com>
2017-10-12 10:29:38 +0200

    Fixed bug in build script when building from master

--- Build.ps1
+++ Build.ps1
@@ -11,7 +11,11 @@ $suffix = @{ $true = ""; $false = "$branch-$revision"}[$branch -eq "master" -and
 foreach ($src in ls src/Serilog.*) {
     Push-Location $src
 
-    & dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$suffix --include-source
+    if($suffix) {
+        & dotnet pack -c Release --include-source --no-build -o ..\..\artifacts --version-suffix=$suffix
+    } else {
+        & dotnet pack -c Release --include-source --no-build -o ..\..\artifacts
+    }
     if($LASTEXITCODE -ne 0) { exit 1 }    
 
     Pop-Location
