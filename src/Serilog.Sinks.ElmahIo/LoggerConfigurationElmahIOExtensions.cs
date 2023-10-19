7821736fbfd56da7797ba6676a8726477c250573
Thomas Ardal <thomasardal@gmail.com>
2023-10-19 19:04:30 +0200

    Re-introduced WriteTo method accepting api key and log id as strings to support configuration through appsettings.json and similar

--- src/Serilog.Sinks.ElmahIo/LoggerConfigurationElmahIOExtensions.cs
+++ src/Serilog.Sinks.ElmahIo/LoggerConfigurationElmahIOExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2014 Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -53,6 +53,33 @@ namespace Serilog
                 batchingSink,
                 restrictedToMinimumLevel: options.MinimumLogEventLevel ?? LevelAlias.Minimum,
                 levelSwitch: options.LevelSwitch);
+        }
+
+        /// <summary>
+        /// Adds a sink that writes log events to elmah.io. This overload accepts logId as a string and
+        /// should be used from packages not supporting ElmahIoSinkOptions (like when configuring
+        /// through appsettings.json files or similar.
+        /// </summary>
+        /// <param name="loggerConfiguration">The logger configuration.</param>
+        /// <param name="apiKey">An API key from the organization containing the log.</param>
+        /// <param name="logId">The log ID as found on the elmah.io website.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum log event level required in order to write an event to the sink. Set to Verbose by default.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Logger configuration, allowing configuration to continue.</returns>
+        /// <exception cref="ArgumentNullException">A required parameter is null.</exception>
+        [Obsolete("This method is intended for configuration through appsettings.json or similar only. From C# you should always call the overload accepting ElmahIoSinkOptions.")]
+        public static LoggerConfiguration ElmahIo(
+            this LoggerSinkConfiguration loggerConfiguration,
+            string apiKey,
+            string logId,
+            LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+            IFormatProvider formatProvider = null)
+        {
+            return ElmahIo(loggerConfiguration, new ElmahIoSinkOptions(apiKey, new Guid(logId))
+            {
+                MinimumLogEventLevel = restrictedToMinimumLevel,
+                FormatProvider = formatProvider,
+            });
         }
     }
 }
---
+++ test/Serilog.Sinks.ElmahIo.Tests/LoggerConfigurationElmahIoExtensionsTest.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Serilog.Core;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Serilog.Sinks.ElmahIo.Tests
+{
+    public class LoggerConfigurationElmahIoExtensionsTest
+    {
+        [Test]
+        public void CanConfigureFromOptions()
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.ElmahIo(new ElmahIoSinkOptions("apiKey", Guid.NewGuid()))
+                .CreateLogger();
+
+            AssertInstalledSink(log);
+        }
+
+        [Test]
+        public void CanConfigureFromStringAndGuid()
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            var log = new LoggerConfiguration()
+                .WriteTo.ElmahIo("apiKey", Guid.NewGuid().ToString())
+                .CreateLogger();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            AssertInstalledSink(log);
+        }
+
+        [Test]
+        public void CanConfigureFromAppsettings()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddObject(new
+                {
+                    Serilog = new
+                    {
+                        Using = new[] { "Serilog.Sinks.ElmahIo" },
+                        MinimumLevel = new
+                        {
+                            Default = "Information"
+                        },
+                        WriteTo = new[]
+                        {
+                            new
+                            {
+                                Name = "ElmahIo",
+                                Args = new
+                                {
+                                    apiKey = "API_KEY",
+                                    logId = Guid.NewGuid().ToString(),
+                                }
+                            }
+                        }
+                    }
+                })
+                .Build();
+
+            var log = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            AssertInstalledSink(log);
+        }
+
+        private static void AssertInstalledSink(Logger log)
+        {
+            var aggregateSinkFieldInfo = log
+                            .GetType()
+                            .GetField("_sink", BindingFlags.Instance | BindingFlags.NonPublic);
+            var aggregateSink = (ILogEventSink)aggregateSinkFieldInfo?.GetValue(log);
+            var sinkEnumerableFieldInfo = aggregateSink?
+                .GetType()
+                .GetField("_sinks", BindingFlags.Instance | BindingFlags.NonPublic);
+            var sinks = (ILogEventSink[])sinkEnumerableFieldInfo?.GetValue(aggregateSink);
+            Assert.That(sinks.Count, Is.EqualTo(1));
+        }
+    }
+}
