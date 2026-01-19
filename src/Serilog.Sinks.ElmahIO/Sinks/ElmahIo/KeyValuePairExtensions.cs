2d354ac698b3e66e7531124dabd988aff8868bff
Thomas Ardal <thomasardal@gmail.com>
2026-01-19 11:00:50 +0100

    Fixed a bug where values in data, server variables, etc. containing start or end quotes would be trimmed

--- src/Serilog.Sinks.ElmahIO/Sinks/ElmahIo/KeyValuePairExtensions.cs
+++ src/Serilog.Sinks.ElmahIO/Sinks/ElmahIo/KeyValuePairExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Elmah.Io.Client;
+using Serilog.Events;
 using System.Collections.Generic;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
@@ -9,13 +10,23 @@ namespace Serilog.Sinks.ElmahIo
     {
         internal static Item ToItem<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair)
         {
-            return new Item(Trim(keyValuePair.Key?.ToString()), Trim(keyValuePair.Value?.ToString()));
+            return new Item(RawValue(keyValuePair.Key), RawValue(keyValuePair.Value));
         }
 
-        private static string Trim(string s)
+        private static string RawValue(object obj)
         {
-            if (string.IsNullOrWhiteSpace(s)) return null;
-            return s.TrimStart('\"').TrimEnd('\"');
+            if (obj == null) return null;
+
+            // Handling of ScalarValue from Serilog
+            if (obj is ScalarValue scalar)
+            {
+                return scalar.Value?.ToString();
+            }
+
+            // For other types, use ToString and treat whitespace as null
+            var result = obj.ToString();
+
+            return string.IsNullOrWhiteSpace(result) ? null : result;
         }
     }
 }
